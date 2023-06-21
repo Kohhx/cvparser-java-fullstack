8a08f3f11d4d3e42991b0c814978dd54b6e1c9fe
James Thiah <jamesthiah@gmail.com>
2023-06-21 20:36:36 +0800

    Resolve merge conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import NavBar from './component/NavBar';
 import Login from './pages/Login.js';
+import ResumeUser from './pages/ResumeUser';
+import Upload from './pages/Upload';
+import { Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Login />
-
+      <Routes>
+        <Route path="/" element={<Login />} />             
+        <Route path="/upload" element={<Upload/>} />
+        <Route path="/resumeuser" element={<ResumeUser/>}/>
+        
+        
+      </Routes>
+      <NavBar/>
     </div>
   );
 }
