d27f88862f9ef8a9f688731d078e4b0c66bd28b9
Emmanuel Lim <limjunhao5714@gmail.com>
2023-06-23 12:53:52 +0800

    Overflow, fixed table sizing

--- frontend/src/pages/UserResumes.js
+++ frontend/src/pages/UserResumes.js
@@ -14,7 +14,7 @@ const UserResumes = () => {
   const [userId, setUserId] = useState();
 
   useEffect(() => {
-    // console.log(ctx.getUserId());
+    console.log(ctx.getUserId());
     setUserId(ctx.userDetails.id);
     resumeAPI.getUserResumes(ctx.getUserId()).then((res) => {
       console.log(res.data);
@@ -26,94 +26,95 @@ const UserResumes = () => {
 
   const candidates = [
     {
-      name: "Mark Lim",
+      filename: "Frontend_Resume",
       skills: "Python, Java, HTML",
       created: "06/12/23",
       details: "#details_20230901",
     },
     {
-      name: "Koh He Xiang",
+      filename: "Backend_Resume",
       skills: "SQL, VBA, HTML",
       created: "06/29/23",
       details: "#details_20230902",
     },
     {
-      name: "James Thiah",
+      filename: "FrontendResume_FINAL_v2_new_updated_redux_imtryingmybest_iswearthisisthefinalupdate",
+      skills: "Python, Java, Ruby",
+      created: "06/01/23",
+      details: "#details_20230915",
+    },
+    {
+      filename: "Emmanuel_FrontEnd_Resume",
       skills: "Logistics, Management",
       created: "03/12/23",
       details: "#details_20230904",
     },
     {
-      name: "Emma Watson",
+      filename: "EmmanuelBackEndResume",
       skills: "Python, Java, C#",
       created: "04/05/23",
       details: "#details_20230905",
     },
     {
-      name: "Tom Cruise",
+      filename: "230623Resume",
       skills: "Project Management, Agile",
       created: "04/15/23",
       details: "#details_20230906",
     },
     {
-      name: "Natalie Portman",
+      filename: "230623",
       skills: "SQL, Python, Java",
       created: "04/18/23",
       details: "#details_20230907",
     },
     {
-      name: "Brad Pitt",
+      filename: "FrontendResume_FINAL",
       skills: "HTML, CSS, JavaScript",
       created: "04/20/23",
       details: "#details_20230908",
     },
     {
-      name: "Scarlett Johansson",
+      filename: "BackendResume_FINAL",
       skills: "Python, Java, Ruby",
       created: "05/03/23",
       details: "#details_20230909",
     },
     {
-      name: "Jennifer Aniston",
+      filename: "FrontendResume_FINAL_v2",
       skills: "PHP, JavaScript, HTML",
       created: "05/09/23",
       details: "#details_20230910",
     },
     {
-      name: "Leonardo DiCaprio",
+      filename: "FrontendResume_FINAL_v2_new",
       skills: "SQL, Python, C#",
       created: "05/10/23",
       details: "#details_20230911",
     },
     {
-      name: "Johnny Depp",
+      filename: "FrontendResume_FINAL_v2_new_updated",
       skills: "Angular, React, Vue.js",
       created: "05/11/23",
       details: "#details_20230912",
     },
     {
-      name: "Keanu Reeves",
+      filename: "FrontendResume_FINAL_v2_new_updated_redux",
       skills: "Project Management, Agile",
       created: "05/15/23",
       details: "#details_20230913",
     },
     {
-      name: "Morgan Freeman",
+      filename: "FrontendResume_FINAL_v2_new_updated_redux_imtryingmybest",
       skills: "HTML, CSS, JavaScript",
       created: "05/20/23",
       details: "#details_20230914",
     },
-    {
-      name: "Robert Downey Jr.",
-      skills: "Python, Java, Ruby",
-      created: "06/01/23",
-      details: "#details_20230915",
-    },
+
   ];
 
   const filteredCandidates = candidates.filter(
     (candidate) =>
-      candidate.name.toLowerCase().includes(searchName.toLowerCase()) &&
+      candidate.filename.toLowerCase().includes(searchName.toLowerCase()) &&
       candidate.skills.toLowerCase().includes(searchSkills.toLowerCase()) &&
       candidate.created.includes(searchDate) // Date match is exact in this case
   );
@@ -127,28 +128,43 @@ const UserResumes = () => {
 
   console.log("filteredCandidates:", filteredCandidates); // Add this line to check filtered candidates in the console.
 
+  //marginTop 80px - Separate it from the NavBar (20 after pulling from global)
   return (
-    <div className="container">
-      <div>
-        <label htmlFor="" className="form-label">Filter Resume Name</label>
-        <input type="text" className="form-control" placeholder="Search by Name"/>
+    <div className="container" >
+      <div className="ml-auto" style={{ marginTop: '80px' }}>
+        <table className="table">
+          <tbody>
+            <tr>
+              <td>
+              <input type="text" placeholder="Search by Resume Filename" style={{width:'100%'}}
+              value={searchName} onChange={e => setSearchName(e.target.value)} />
+              </td>
+            </tr>
+            <tr>
+            <td>
+            <input type="text" placeholder="Search by Skills" style={{width:'100%'}}
+            value={searchSkills} onChange={e => setSearchSkills(e.target.value)} />
+            </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
 
-      <table className="table table-bordered">
+      <table className="table table-responsive-sm table-fixed" >
         <thead>
           <tr className="table-active table-primary">
-            <th scope="col">Candidate</th>
+            <th scope="col">Resume Filename</th>
             <th scope="col">Skills</th>
             <th scope="col">Created on</th>
             <th scope="col">User details</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody >
           {filteredCandidates
             .slice(offset, offset + PER_PAGE)
             .map((candidate) => (
-              <tr key={candidate.name}>
-                <th scope="row">{candidate.name}</th>
+              <tr key={candidate.filename}>
+                <th scope="row" >{candidate.filename}</th>
                 <td>{candidate.skills}</td>
                 <td>{candidate.created}</td>
                 <td>
@@ -158,7 +174,8 @@ const UserResumes = () => {
             ))}
         </tbody>
       </table>
-
+            
+      <div>
       <ReactPaginate
         previousLabel={"←"}
         nextLabel={"→"}
@@ -169,44 +186,26 @@ const UserResumes = () => {
         nextLinkClassName={"pagination__link"}
         disabledClassName={"pagination__link--disabled"}
         activeClassName={"pagination__link--active"}
+        style={{
+          width:`${80*pageCount}px`
+        }}
       />
-
-      {/* <div className="ml-auto right-side">
-                <table className="table">
-                    <tbody>
-                        <tr>
-                            <td>
-                                <label>Name: </label>
-                            </td>
-                            <td>
-                                <input type="text" placeholder="Search by Name" value={searchName} onChange={e => setSearchName(e.target.value)} />
-                            </td>
-                            </tr>
-                            <tr>
-                            <td>
-                                <label>Skills: </label>
-                            </td>
-                            <td>
-                                <input type="text" placeholder="Search by Skills" value={searchSkills} onChange={e => setSearchSkills(e.target.value)} />
-                            </td>
-
-                            </tr>
-                            <tr>
-                            <td>
-                                <label>Date: </label>
-                            </td>
-                            <td>
-                                <input type="text" placeholder="Search by Date" value={searchDate} onChange={e => setSearchDate(e.target.value)} />
-                            </td>
-
-                            </tr>
-
-
-                    </tbody>
-                </table>
-            </div> */}
+      </div>
     </div>
   );
 };
+
+
+
+                            // <tr>
+                            // <td>
+                            //     <label>Date: </label>
+                            // </td>
+                            // <td>
+                            //     <input type="text" placeholder="Search by Date" value={searchDate} onChange={e => setSearchDate(e.target.value)} />
+                            // </td>
+
+                            // </tr>
+
 
 export default UserResumes;
