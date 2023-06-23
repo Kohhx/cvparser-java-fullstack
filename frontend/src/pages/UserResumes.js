6c3408fed8e7601701f37cfee6d4f9756a359df6
Emmanuel Lim <limjunhao5714@gmail.com>
2023-06-23 18:39:31 +0800

    Update UserResumes css
    Need to fix border-radius

--- frontend/src/pages/UserResumes.js
+++ frontend/src/pages/UserResumes.js
@@ -31,85 +31,85 @@ const UserResumes = () => {
       filename: "Frontend_Resume",
       skills: ["Python", "Java", "HTML"],
       created: "2023-06-12 00:00:00.000000",
-      details: "#details_20230901",
+      details: "/1",
     },
     {
       filename: "Backend_Resume",
       skills: ["SQL", "VBA", "HTML"],
       created: "06/29/23",
-      details: "#details_20230902",
+      details: "/2",
     },
     {
       filename: "FrontendResume_FINAL_v2_new_updated_redux_imtryingmybest_iswearthisisthefinalupdate",
       skills: ["Python", "Java", "Ruby"],
       created: "06/01/23",
-      details: "#details_20230915",
+      details: "/3",
     },
     {
       filename: "Emmanuel_FrontEnd_Resume",
       skills: ["Logistics","Management","Python","Java","HTML","SQL","VBA","Project Management","Agile"],
       created: "03/12/23",
-      details: "#details_20230904",
+      details: "/4",
     },
     {
       filename: "EmmanuelBackEndResume",
       skills: ["Python", "Java", "C#"],
       created: "04/05/23",
-      details: "#details_20230905",
+      details: "/5",
     },
     {
       filename: "230623Resume",
       skills: ["Project Management", "Agile"],
       created: "04/15/23",
-      details: "#details_20230906",
+      details: "/6",
     },
     {
       filename: "230623",
       skills: ["SQL", "Python", "Java"],
       created: "04/18/23",
-      details: "#details_20230907",
+      details: "/7",
     },
     {
       filename: "FrontendResume_FINAL",
       skills: ["HTML", "CSS", "JavaScript"],
       created: "04/20/23",
-      details: "#details_20230908",
+      details: "/8",
     },
     {
       filename: "BackendResume_FINAL",
       skills: ["Python", "Java", "Ruby"],
       created: "05/03/23",
-      details: "#details_20230909",
+      details: "/9",
     },
     {
       filename: "FrontendResume_FINAL_v2",
       skills: ["PHP", "JavaScript", "HTML"],
       created: "05/09/23",
-      details: "#details_20230910",
+      details: "/10",
     },
     {
       filename: "FrontendResume_FINAL_v2_new",
       skills: ["SQL", "Python", "C#"],
       created: "05/10/23",
-      details: "#details_20230911",
+      details: "/11",
     },
     {
       filename: "FrontendResume_FINAL_v2_new_updated",
       skills: ["Angular", "React", "Vue.js"],
       created: "05/11/23",
-      details: "#details_20230912",
+      details: "/12",
     },
     {
       filename: "FrontendResume_FINAL_v2_new_updated_redux",
       skills: ["Project Management", "Agile"],
       created: "05/15/23",
-      details: "#details_20230913",
+      details: "/13",
     },
     {
       filename: "FrontendResume_FINAL_v2_new_updated_redux_imtryingmybest",
       skills: ["HTML", "CSS", "JavaScript"],
       created: "05/20/23",
-      details: "#details_20230914",
+      details: "/14",
     },
   ];
   
@@ -132,21 +132,22 @@ const UserResumes = () => {
   console.log("filteredCandidates:", filteredCandidates); // Add this line to check filtered candidates in the console.
 
   return (
-    <div className="container" >
-      <div className="ml-auto">
+    <div className="container" style={{display:'flex', justifyContent:'center'}} >
+      <div style={{width:'85%'}}>
+      <div className="ml-auto ">
         <table className="table">
           <tbody>
             <tr>
-              <td>
-              <input type="text" placeholder="Search by Resume Filename" style={{width:'100%'}}
+              <td style={{padding:'15px'}}>
+              <input type="text" placeholder="Search by Resume Filename" 
               value={searchName} onChange={e => setSearchName(e.target.value)} />
               </td>
             </tr>
             <tr>
-            <td>
-            <input type="text" placeholder="Search by Skills" style={{width:'100%'}}
-            value={searchSkills} onChange={e => setSearchSkills(e.target.value)} />
-            </td>
+              <td style={{padding:'15px'}}>
+              <input type="text" placeholder="Search by Skills" 
+              value={searchSkills} onChange={e => setSearchSkills(e.target.value)} />
+              </td>
             </tr>
           </tbody>
         </table>
@@ -155,8 +156,8 @@ const UserResumes = () => {
       <table className="table table-responsive-sm table-fixed" >
         <thead>
           <tr className="table-active table-primary">
-            <th scope="col">Resume Filename</th>
-            <th scope="col">Skills</th>
+            <th scope="col" colspan="2">Filename</th>
+            <th scope="col" colspan="4">Skills</th>
             <th scope="col">Created on</th>
             <th scope="col" colspan="2"></th>
           </tr>
@@ -176,8 +177,8 @@ const UserResumes = () => {
 
             return (
               <tr key={candidate.filename}>
-                <th scope="row">{candidate.filename}</th>
-                <td>{candidate.skills.join(", ")}</td>
+                <th scope="row" colspan="2">{candidate.filename}</th>
+                <td colspan="4">{candidate.skills.join(", ")}</td>
                 <td>{formattedCreatedAt}</td>
                 <td style={{textAlign:"center"}}>
                   {/* Change to YourResume link */}
@@ -213,6 +214,7 @@ const UserResumes = () => {
           width:`${80*pageCount}px`
         }}
       />
+      </div>
       </div>
     </div>
   );
