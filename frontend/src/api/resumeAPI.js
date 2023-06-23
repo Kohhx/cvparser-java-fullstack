a95e992250220fed49c3cb4a04c28a9eede786d7
He Xiang Koh <kohhxx6@gmail.com>
2023-06-23 16:30:52 +0800

    Added get, update and delete a resume

--- frontend/src/api/resumeAPI.js
+++ frontend/src/api/resumeAPI.js
@@ -3,7 +3,7 @@ import { toast } from "react-toastify";
 
 export const resumeAPI = {
   uploadResume: async (resumeDetails) => {
-     return axiosInstance
+    return axiosInstance
       .post("resumes", resumeDetails)
       .then((res) => {
         toast.success("Resume uploaded successfully.");
@@ -11,10 +11,19 @@ export const resumeAPI = {
       })
       .catch((err) => {
         console.log(err);
-        toast.error("Error in uploading resume. Please try again.")
+        toast.error("Error in uploading resume. Please try again.");
       });
   },
   getUserResumes: async (userId) => {
-      return axiosInstance.get(`users/${userId}/resumes`);
+    return axiosInstance.get(`users/${userId}/resumes`);
+  },
+  getResume: async (userId, resume) => {
+    return axiosInstance.get(`users/${userId}/resumes/${resume}`);
+  },
+  updateResume: async (resumeDetails) => {
+    return axiosInstance.patch(`resumes/${resumeDetails.id}`, resumeDetails);
+  },
+  deleteResume: async (resumeId) => {
+    return axiosInstance.delete(`resumes/${resumeId}`);
   }
 };
