34eabededeb3e63146960d09225f31bb208f8ce3
He Xiang Koh <kohhxx6@gmail.com>
2023-06-23 10:14:08 +0800

    add route guard and fix nav bar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { UserContext, value } from "./context/userContext";
+import UserProvider from "./context/UserProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <UserContext.Provider value={value}>
+  <UserProvider>
     <App />
-  </UserContext.Provider>
+  </UserProvider>
 );
