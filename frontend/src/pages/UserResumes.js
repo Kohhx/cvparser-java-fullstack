0c5f11d0b0650ef2307ddb3be32aa1bbeb998d97
Emmanuel Lim <limjunhao5714@gmail.com>
2023-06-23 14:45:26 +0800

    Imported Icons, requires details and delete functions only
    Skills string is now an array of skills,

--- frontend/src/pages/UserResumes.js
+++ frontend/src/pages/UserResumes.js
@@ -4,14 +4,16 @@ import "bootstrap/dist/css/bootstrap.css";
 import ReactPaginate from "react-paginate";
 import { resumeAPI } from "../api/resumeAPI";
 import { UserContext } from "../context/userContext";
-
+import detailsIcon from "./Assets/detailsIcon.png";
+import deleteIcon from "./Assets/deleteIcon.png";
 const UserResumes = () => {
   const ctx = useContext(UserContext);
-
   const [searchName, setSearchName] = useState("");
   const [searchSkills, setSearchSkills] = useState("");
   const [searchDate, setSearchDate] = useState("");
   const [userId, setUserId] = useState();
+
+
 
   useEffect(() => {
     console.log(ctx.getUserId());
@@ -27,97 +29,98 @@ const UserResumes = () => {
   const candidates = [
     {
       filename: "Frontend_Resume",
-      skills: "Python, Java, HTML",
-      created: "06/12/23",
+      skills: ["Python", "Java", "HTML"],
+      created: "2023-06-12 00:00:00.000000",
       details: "#details_20230901",
     },
     {
       filename: "Backend_Resume",
-      skills: "SQL, VBA, HTML",
+      skills: ["SQL", "VBA", "HTML"],
       created: "06/29/23",
       details: "#details_20230902",
     },
     {
       filename: "FrontendResume_FINAL_v2_new_updated_redux_imtryingmybest_iswearthisisthefinalupdate",
-      skills: "Python, Java, Ruby",
+      skills: ["Python", "Java", "Ruby"],
       created: "06/01/23",
       details: "#details_20230915",
     },
     {
       filename: "Emmanuel_FrontEnd_Resume",
-      skills: "Logistics, Management",
+      skills: ["Logistics","Management","Python","Java","HTML","SQL","VBA","Project Management","Agile"],
       created: "03/12/23",
       details: "#details_20230904",
     },
     {
       filename: "EmmanuelBackEndResume",
-      skills: "Python, Java, C#",
+      skills: ["Python", "Java", "C#"],
       created: "04/05/23",
       details: "#details_20230905",
     },
     {
       filename: "230623Resume",
-      skills: "Project Management, Agile",
+      skills: ["Project Management", "Agile"],
       created: "04/15/23",
       details: "#details_20230906",
     },
     {
       filename: "230623",
-      skills: "SQL, Python, Java",
+      skills: ["SQL", "Python", "Java"],
       created: "04/18/23",
       details: "#details_20230907",
     },
     {
       filename: "FrontendResume_FINAL",
-      skills: "HTML, CSS, JavaScript",
+      skills: ["HTML", "CSS", "JavaScript"],
       created: "04/20/23",
       details: "#details_20230908",
     },
     {
       filename: "BackendResume_FINAL",
-      skills: "Python, Java, Ruby",
+      skills: ["Python", "Java", "Ruby"],
       created: "05/03/23",
       details: "#details_20230909",
     },
     {
       filename: "FrontendResume_FINAL_v2",
-      skills: "PHP, JavaScript, HTML",
+      skills: ["PHP", "JavaScript", "HTML"],
       created: "05/09/23",
       details: "#details_20230910",
     },
     {
       filename: "FrontendResume_FINAL_v2_new",
-      skills: "SQL, Python, C#",
+      skills: ["SQL", "Python", "C#"],
       created: "05/10/23",
       details: "#details_20230911",
     },
     {
       filename: "FrontendResume_FINAL_v2_new_updated",
-      skills: "Angular, React, Vue.js",
+      skills: ["Angular", "React", "Vue.js"],
       created: "05/11/23",
       details: "#details_20230912",
     },
     {
       filename: "FrontendResume_FINAL_v2_new_updated_redux",
-      skills: "Project Management, Agile",
+      skills: ["Project Management", "Agile"],
       created: "05/15/23",
       details: "#details_20230913",
     },
     {
       filename: "FrontendResume_FINAL_v2_new_updated_redux_imtryingmybest",
-      skills: "HTML, CSS, JavaScript",
+      skills: ["HTML", "CSS", "JavaScript"],
       created: "05/20/23",
       details: "#details_20230914",
     },
-
   ];
-
-  const filteredCandidates = candidates.filter(
-    (candidate) =>
+  
+  const filteredCandidates = candidates.filter((candidate) => {
+    return (
+      candidate.skills.some((skill) => skill.toLowerCase().includes(searchSkills.toLowerCase())) &&
       candidate.filename.toLowerCase().includes(searchName.toLowerCase()) &&
-      candidate.skills.toLowerCase().includes(searchSkills.toLowerCase()) &&
       candidate.created.includes(searchDate) // Date match is exact in this case
-  );
+    )
+    });
+
 
   function handlePageClick({ selected: selectedPage }) {
     setCurrentPage(selectedPage);
@@ -128,7 +131,6 @@ const UserResumes = () => {
 
   console.log("filteredCandidates:", filteredCandidates); // Add this line to check filtered candidates in the console.
 
-  //marginTop 80px - Separate it from the NavBar (20 after pulling from global)
   return (
     <div className="container" >
       <div className="ml-auto" style={{ marginTop: '80px' }}>
@@ -156,22 +158,43 @@ const UserResumes = () => {
             <th scope="col">Resume Filename</th>
             <th scope="col">Skills</th>
             <th scope="col">Created on</th>
-            <th scope="col">User details</th>
+            <th scope="col" colspan="2"></th>
           </tr>
         </thead>
         <tbody >
-          {filteredCandidates
-            .slice(offset, offset + PER_PAGE)
-            .map((candidate) => (
+        {filteredCandidates
+          .slice(offset, offset + PER_PAGE)
+          .map((candidate) => {
+            //Custom formatting for YYYY-MM-DD HH:MM
+            const createdAt = new Date(candidate.created);
+            const year = createdAt.getFullYear();
+            const month = String(createdAt.getMonth() + 1).padStart(2, "0");
+            const day = String(createdAt.getDate()).padStart(2, "0");
+            const hours = String(createdAt.getHours()).padStart(2, "0");
+            const minutes = String(createdAt.getMinutes()).padStart(2, "0");
+            const formattedCreatedAt = `${year}-${month}-${day} ${hours}:${minutes}`;
+
+            return (
               <tr key={candidate.filename}>
-                <th scope="row" >{candidate.filename}</th>
-                <td>{candidate.skills}</td>
-                <td>{candidate.created}</td>
-                <td>
-                  <a href={candidate.details}>Details</a>
+                <th scope="row">{candidate.filename}</th>
+                <td>{candidate.skills.join(", ")}</td>
+                <td>{formattedCreatedAt}</td>
+                <td style={{textAlign:"center"}}>
+                  {/* Change to YourResume link */}
+                  <a href={candidate.details}>
+                    <img src={detailsIcon} alt="Details" style={{ width: '24px', height: '24px'}}></img>
+                  </a>
+                </td>
+                <td style={{textAlign:"center"}}>
+                  {/* Change to YourResume link */}
+                  <a href={candidate.details}>
+                    <img src={deleteIcon} alt="Details" style={{ width: '24px', height: '24px'}}></img>
+                  </a>
                 </td>
               </tr>
-            ))}
+            );
+          })
+        }
         </tbody>
       </table>
             
