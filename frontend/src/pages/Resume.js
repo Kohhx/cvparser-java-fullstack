7526ae61b22e9faf27c1d69cefac74bf6b858983
He Xiang Koh <kohhxx6@gmail.com>
2023-08-02 17:01:20 +0800

    Added last job title and can replace all enull, undefined with - for word report

--- frontend/src/pages/Resume.js
+++ frontend/src/pages/Resume.js
@@ -793,6 +793,37 @@ const Resume = () => {
             </div>
           )}
 
+          {/* Last Job Title */}
+          <div className="flex-form">
+            <div class="resume-details-part w-100">
+              <div className="d-flex align-items-center gap-2">
+                <h3>Last Job Title</h3>
+                <AiFillEdit
+                  className="edit-icons-md"
+                  onClick={() =>
+                    setJobTitle((prev) => ({
+                      ...prev,
+                      isEditing: !prev.isEditing,
+                    }))
+                  }
+                />
+              </div>
+              <div className="d-flex align-items-center justify-content-between gap-5">
+                <input
+                  type="text"
+                  value={jobTitle.value}
+                  onChange={(e) =>
+                    setJobTitle((prev) => ({
+                      ...prev,
+                      value: e.target.value,
+                    }))
+                  }
+                  disabled={!jobTitle.isEditing}
+                />
+              </div>
+            </div>
+          </div>
+
           {/* Spoken Languages */}
           <div class="resume-skills-section">
             <div className="d-flex align-items-center gap-2">
